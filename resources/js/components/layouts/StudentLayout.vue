a0b5f44d33b6c352929d7d252aecdbff9059e36b
akpanphilip <akpanphilip1122@gmail.com>
2024-06-26 17:37:32 -0800

    bottom navigation

--- resources/js/components/layouts/StudentLayout.vue
+++ resources/js/components/layouts/StudentLayout.vue
@@ -1,64 +1,78 @@
 <template>
-    <v-card>
-        <v-row>
-            <v-col cols="9">
-                <h1 class="pa-6">Student Layout</h1>
-                <router-view></router-view>
-            </v-col>
-            <v-col cols="3">
-                <v-layout>
-                    <v-navigation-drawer location="right" permanent>
-                        <template v-slot:prepend>
-                            <v-list-item
-                                lines="two"
-                                prepend-avatar="https://randomuser.me/api/portraits/women/81.jpg"
-                            >
-                                {{ user.name }} <br />
-                                <small>{{ user.email }}</small>
-                            </v-list-item>
-                        </template>
+    <v-responsive class="border rounded">
+        <v-app>
+            <v-navigation-drawer>
+                <template v-slot:prepend>
+                    <v-list-item
+                        lines="two"
+                        prepend-avatar="https://randomuser.me/api/portraits/women/81.jpg"
+                    >
+                        {{ user.name }} <br />
+                        <small>{{ user.email }}</small>
+                    </v-list-item>
+                </template>
 
-                        <v-divider></v-divider>
+                <v-divider></v-divider>
+                <v-list>
+                    <v-list-item
+                        :class="{
+                            'v-list-item--active': isActive('studentIndex'),
+                        }"
+                        prepend-icon="mdi-home-city"
+                    >
+                        <router-link :to="{ name: 'studentIndex' }">
+                            Dashboard
+                        </router-link>
+                    </v-list-item>
+                    <v-list-item
+                        :class="{
+                            'v-list-item--active': isActive(
+                                'studentProfileSetting'
+                            ),
+                        }"
+                        prepend-icon="mdi-account"
+                    >
+                        <router-link :to="{ name: 'studentProfileSetting' }">
+                            Settings
+                        </router-link>
+                    </v-list-item>
+                    <v-list-item
+                        @click="logout"
+                        prepend-icon="mdi-logout"
+                        title="Logout"
+                        value="logout"
+                    ></v-list-item>
+                </v-list>
+            </v-navigation-drawer>
 
-                        <v-list density="compact" nav>
-                            <v-list-item
-                                :class="{
-                                    'v-list-item--active':
-                                        isActive('studentIndex'),
-                                }"
-                                prepend-icon="mdi-home-city"
-                            >
-                                <router-link :to="{ name: 'studentIndex' }">
-                                    Dashboard
-                                </router-link>
-                            </v-list-item>
-                            <v-list-item
-                                :class="{
-                                    'v-list-item--active': isActive(
-                                        'studentProfileSetting'
-                                    ),
-                                }"
-                                prepend-icon="mdi-account"
-                            >
-                                <router-link
-                                    :to="{ name: 'studentProfileSetting' }"
-                                >
-                                    Settings
-                                </router-link>
-                            </v-list-item>
-                            <v-list-item
-                                @click="logout"
-                                prepend-icon="mdi-logout"
-                                title="Logout"
-                                value="logout"
-                            ></v-list-item>
-                        </v-list>
-                    </v-navigation-drawer>
-                    <v-main style="height: 250px"></v-main>
-                </v-layout>
-            </v-col>
-        </v-row>
-    </v-card>
+            <v-app-bar></v-app-bar>
+
+            <v-main>
+                <v-container>
+                    <router-view></router-view>
+                </v-container>
+            </v-main>
+            <v-bottom-navigation class="d-none d-sm-flex d-md-none" grow>
+                <v-btn value="recent">
+                    <v-icon>mdi-history</v-icon>
+
+                    <span>Recent</span>
+                </v-btn>
+
+                <v-btn value="favorites">
+                    <v-icon>mdi-heart</v-icon>
+
+                    <span>Favorites</span>
+                </v-btn>
+
+                <v-btn value="nearby">
+                    <v-icon>mdi-map-marker</v-icon>
+
+                    <span>Nearby</span>
+                </v-btn>
+            </v-bottom-navigation>
+        </v-app>
+    </v-responsive>
 </template>
 <script>
 import { mapActions } from "vuex";
